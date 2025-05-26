660a85b2567d9003f21c7a8a386d088f164f91db
techizone <venkateshsap413@gmail.com>
2025-05-26 09:24:40 +0530

    Added Parameter LAB files

---
+++ Jenkins-Test-Parameters-LAB/src/test/java/tests/test1.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+	@Test
+	public void testcase1() {
+		System.out.println("#########################################");
+		System.out.println("#      Jenkins Parameters Session       #");
+		System.out.println("#########################################");
+		System.out.println("#########################################");
+		System.out.println("#               master BRANCH           #");
+		System.out.println("#########################################");
+
+	}
+}
