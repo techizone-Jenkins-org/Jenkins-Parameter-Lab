b8857cf5004e8b981813dd2c52183977175db72c
techizone <venkateshsap413@gmail.com>
2025-05-30 20:35:47 +0530

    Added maven Build Artifact LAB

---
+++ 01.Maven-Build-Artifact/02.PassParametersRunTimeViaMVN/Real-Time_Use-case/mavenprofiles/src/test/java/regressionTestcases/RegressionTest3.java
@@ -0,0 +1,15 @@
+ package regressionTestcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RegressionTest3 {
+	
+	@Test
+	public void m1() {		
+		System.out.println("------------ Regression Test cases 3 ---------------");		
+	}
+}
