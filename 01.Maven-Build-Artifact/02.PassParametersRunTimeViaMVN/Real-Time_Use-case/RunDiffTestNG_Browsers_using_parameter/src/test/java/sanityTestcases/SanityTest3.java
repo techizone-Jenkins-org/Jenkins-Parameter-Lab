c98be0c54e0362590766adb0bb3dd4e95b85d393
techizone <venkateshsap413@gmail.com>
2025-05-30 22:40:02 +0530

    Added maven Build Artifact LAB

---
+++ 01.Maven-Build-Artifact/02.PassParametersRunTimeViaMVN/Real-Time_Use-case/RunDiffTestNG_Browsers_using_parameter/src/test/java/sanityTestcases/SanityTest3.java
@@ -0,0 +1,15 @@
+ package sanityTestcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SanityTest3 {
+	
+	@Test
+	public void m1() {		
+		System.out.println("------------ Sanity Test cases 3 ---------------");		
+	}
+}
